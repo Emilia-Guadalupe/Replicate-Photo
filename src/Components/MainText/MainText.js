4de17cdadb2cca9b9a91032d3c61367734d9a8d6
Emilia <m.emilia.guadalupe@gmail.com>
2021-04-01 12:33:01 -0300

    Last part

--- src/Components/MainText/MainText.js
+++ src/Components/MainText/MainText.js
@@ -6,7 +6,7 @@ function MainText(){
         <div id="textContainer">
             <h1 id="mainTitle">Rich Places. Personal Photo Project</h1>
             <p className="main_text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-            <p id="link">Read More</p>
+            <button id="link">Read More</button>
         </div>
     )
 }
